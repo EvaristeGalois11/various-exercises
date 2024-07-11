29b2a4b6ecef61831d157a0e5a0a882e650ef20c
Claudio Nave <evaristegalois11@proton.me>
2024-07-11 20:29:07 +0200

    Apply spotless

--- ip-report/src/main/java/org/example/ipreport/writer/StatisticJsonWriter.java
+++ ip-report/src/main/java/org/example/ipreport/writer/StatisticJsonWriter.java
@@ -1,3 +1,19 @@
+/*
+ *Copyright (C) 2024 Claudio Nave
+ *
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.example.ipreport.writer;
 
 import java.io.IOException;
@@ -20,7 +36,8 @@ public class StatisticJsonWriter implements StatisticWriter {
 
   @Override
   public void writeStatistics(List<Statistic> statistics, Path path) throws IOException {
-    var json = statistics.stream().map(this::toJson).collect(Collectors.joining(",\n", "[\n", "\n]"));
+    var json =
+        statistics.stream().map(this::toJson).collect(Collectors.joining(",\n", "[\n", "\n]"));
     Files.writeString(path, json);
   }
 
